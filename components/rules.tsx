c6ef99b1aa4902a2be339425b3efa91a60a18fb6
Gerrit Plehn <gerritplehn@gmail.com>
2022-09-13 23:52:47 +0200

    Rule update

--- components/rules.tsx
+++ components/rules.tsx
@@ -3,9 +3,8 @@ export default function Rules() {
     <div>
       <h2>Rules</h2>
       <p>There is no limit on how many games you can play or scores you can submit!</p>
-      <p>You may not enable Stride mode.</p>
       <p>The score has to be set in the specified time range.</p>
-      <p>You must submit the score before the timer runs out.</p>
+      <p>You have 8 hours after the competition time window to submit scores.</p>
       <p>Only the best score of every person counts.</p>
       <h2>How to participate</h2>
       <p>Go to <a href="https://tetr.io" target={'_blank'} rel="noreferrer">TETR.IO</a></p>
