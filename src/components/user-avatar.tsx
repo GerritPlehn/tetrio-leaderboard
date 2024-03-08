8c795f47308f0a1b0610e31f7f8daae47e090933
Gerrit Plehn <gerrit@plehn.me>
2024-03-08 11:24:35 +0100

    user nav

---
+++ src/components/user-avatar.tsx
@@ -0,0 +1,24 @@
+import { type User } from "next-auth";
+import { type AvatarProps } from "@radix-ui/react-avatar";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Icons } from "@/components/icons";
+
+interface UserAvatarProps extends AvatarProps {
+  user: Pick<User, "image" | "name">;
+}
+
+export function UserAvatar({ user, ...props }: UserAvatarProps) {
+  return (
+    <Avatar {...props}>
+      {user.image ? (
+        <AvatarImage alt="User Profile Picture" src={user.image} />
+      ) : (
+        <AvatarFallback>
+          <span className="sr-only">{user.name}</span>
+          <Icons.user className="h-4 w-4" />
+        </AvatarFallback>
+      )}
+    </Avatar>
+  );
+}
